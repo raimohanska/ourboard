380b575920dcf1e091af9cbee5b8d31b782a3d1d
Juha Paananen <juha.paananen@gmail.com>
2020-10-31 19:47:07 +0200

    copy-paste containers with children

--- frontend/src/board/cut-copy-paste.ts
+++ frontend/src/board/cut-copy-paste.ts
@@ -12,12 +12,30 @@ export function cutCopyPasteHandler(board: L.Property<Board>, focus: L.Atom<Boar
 
     let clipboard: Item[] = [];
 
-    const makeCopy = (xDiff: number, yDiff: number) => (i: Item) => {
-        switch (i.type) {
-            case "image": return { ...i, id: uuid.v4(), x: i.x + xDiff, y: i.y + yDiff }
-            case "note": return { ...i, id: uuid.v4(), text: i.text, x: i.x + xDiff, y: i.y + yDiff }
-            default: throw Error("Unsupported item")
-        }    
+    const makeCopy = (items: Item[], board: Board, xDiff: number, yDiff: number): { toCreate: Item[], toSelect: Item[] } => {
+        const containedItemIds = new Set(items.flatMap(i => i.type === "container" ? i.items : []))
+            
+        const withoutContainedItems = items.filter(i => !containedItemIds.has(i.id))
+
+        console.log("rootitems", withoutContainedItems)
+
+        let toCreate: Item[] = []
+        const toSelect = withoutContainedItems.map(makeCopy)
+        toCreate = [...toCreate, ...toSelect]
+        return { toCreate, toSelect }
+
+        function makeCopy(i: Item): Item {
+            switch (i.type) {
+                case "container": 
+                    const copiesOfContained = i.items.map(id => board.items.find(i => i.id === id)!).flatMap(makeCopy)
+                    toCreate = [...toCreate, ...copiesOfContained]
+                    const copyContainer = { ...i, id: uuid.v4(), x: i.x + xDiff, y: i.y + yDiff, items: copiesOfContained.map(i => i.id) }
+                    return copyContainer
+    
+                default:
+                    return { ...i, id: uuid.v4(), x: i.x + xDiff, y: i.y + yDiff }
+            } 
+        }
     }
 
     CLIPBOARD_EVENTS.forEach(eventType => {
@@ -45,10 +63,9 @@ export function cutCopyPasteHandler(board: L.Property<Board>, focus: L.Atom<Boar
                     const currentCenter = coordinateHelper.currentBoardCoordinates.get()
                     const xDiff = currentCenter.x - xCenterOld
                     const yDiff = currentCenter.y - yCenterOld
-                    const copies = clipboard.map(makeCopy(xDiff, yDiff))
-                    
-                    copies.forEach(it => dispatch({ action: "item.add", boardId: currentBoard.id, item: it }))
-                    focus.set({ status: "selected", ids: copies.map(it => it.id) })
+                    const { toCreate, toSelect } = makeCopy(clipboard, board.get(), xDiff, yDiff)                    
+                    toCreate.forEach(it => dispatch({ action: "item.add", boardId: currentBoard.id, item: it }))
+                    focus.set({ status: "selected", ids: toSelect.map(it => it.id) })
                     break
                 }
             }
