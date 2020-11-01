eaa206544dea6467d22b7595876229901cb60665
Juha Paananen <juha.paananen@gmail.com>
2020-11-01 14:00:43 +0200

    first stab at autosizing font

--- frontend/src/board/ItemView.tsx
+++ frontend/src/board/ItemView.tsx
@@ -1,5 +1,5 @@
 import * as H from "harmaja";
-import { h } from "harmaja";
+import { componentScope, h } from "harmaja";
 import * as L from "lonna";
 import { BoardCoordinateHelper } from "./board-coordinates"
 import { Board, Id, Note, ItemLocks, Item, Text, ItemType } from "../../../common/domain";
@@ -22,8 +22,13 @@ export const ItemView = (
         contextMenu: L.Atom<ContextMenu>
     }
 ) => {
+  const element = L.atom<HTMLElement | null>(null)
+  const ref = (el: HTMLElement) => {
+     itemDragToMove(id, board, focus, coordinateHelper, dispatch)(el)
+     element.set(el)
+  }
 
-  const ref = itemDragToMove(id, board, focus, coordinateHelper, dispatch)
+
 
   const { itemFocus, selected, onClick } = itemSelectionHandler(id, focus, contextMenu, board, userId, locks, dispatch)
 
@@ -58,7 +63,7 @@ export const ItemView = (
         height: p.height + "em",
         width: p.width + "em",
         background: p.type === "note" ? p.color : "none",
-        position: "absolute"
+        position: "absolute"        
       })))}      
     >
       { (type === "note" || type === "text") ? <TextView item={item as L.Property<Note | Text>}/> : null }
@@ -70,8 +75,24 @@ export const ItemView = (
   function TextView({ item } : { item: L.Property<Note | Text>} ) {
     const textAtom = L.atom(L.view(item, "text"), text => dispatch({ action: "item.update", boardId: board.get().id, item: { ...item.get(), text } }))
     const showCoords = false
+    const textElement = L.atom<HTMLElement | null>(null)
   
-    
+    const fontSize = L.view(item, element, textElement, (i: Item, el: HTMLElement | null, textEl: HTMLElement | null) => {
+      if (el && textEl) {
+        // TODO: not stable. Would make sense to calculate font while editing and then store it in the model to make it consistent. 
+        const [_, n, unit] = getComputedStyle(textEl).getPropertyValue('font-size').match(/([\d\.]*)(.*)/)        
+        const currentFontSize = parseFloat(n)
+        const textWidth = textEl.getBoundingClientRect().width
+        const elementWidth = el.getBoundingClientRect().width
+        const textHeight = textEl.getBoundingClientRect().height
+        const elementHeight = el.getBoundingClientRect().width
+        const fontSize = (Math.min(currentFontSize * elementWidth / textWidth, currentFontSize * elementHeight / textHeight) * 0.8).toFixed(2)
+        console.log(`${textAtom.get()} current ${currentFontSize}, target ${fontSize}, width ${textWidth}/${elementWidth}, height ${textHeight}/${elementHeight}`)
+        return fontSize + unit
+      }
+      return "1em"
+    })
+
     const setEditingIfAllowed = (e: boolean) => {
       const l = locks.get()
       const u = userId.get()
@@ -83,7 +104,7 @@ export const ItemView = (
       !l[id] && dispatch({ action: "item.lock", boardId: board.get().id, itemId: id })
     }
   
-    return <span className="text">
+    return <span className="text" ref={(e : HTMLElement) => setTimeout(() => textElement.set(e), 0)} style={ L.combineTemplate({fontSize}) }>
       <EditableSpan {...{
         value: textAtom, editingThis: L.atom(
             L.view(itemFocus, f => f === "editing"),
