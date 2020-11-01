2a92ee3c0c3d1676b9185f4629ae352f06b385c5
Juha Paananen <juha.paananen@gmail.com>
2020-11-01 17:07:15 +0200

    Font size logic based on static factors

--- frontend/src/board/ItemView.tsx
+++ frontend/src/board/ItemView.tsx
@@ -10,6 +10,7 @@ import { SelectionBorder } from "./SelectionBorder"
 import { itemDragToMove } from "./item-dragmove"
 import { itemSelectionHandler } from "./item-selection";
 import { Dispatch } from "./board-store";
+import _ from "lodash"
 
 export const ItemView = (
     { board, id, type, item, locks, userId, focus, coordinateHelper, dispatch, contextMenu }:
@@ -75,22 +76,19 @@ export const ItemView = (
   function TextView({ item } : { item: L.Property<Note | Text>} ) {
     const textAtom = L.atom(L.view(item, "text"), text => dispatch({ action: "item.update", boardId: board.get().id, item: { ...item.get(), text } }))
     const showCoords = false
-    const textElement = L.atom<HTMLElement | null>(null)
   
-    const fontSize = L.view(item, element, textElement, (i: Item, el: HTMLElement | null, textEl: HTMLElement | null) => {
-      if (el && textEl) {
-        // TODO: not stable. Would make sense to calculate font while editing and then store it in the model to make it consistent. 
-        const [_, n, unit] = getComputedStyle(textEl).getPropertyValue('font-size').match(/([\d\.]*)(.*)/)        
-        const currentFontSize = parseFloat(n)
-        const textWidth = textEl.getBoundingClientRect().width
-        const elementWidth = el.getBoundingClientRect().width
-        const textHeight = textEl.getBoundingClientRect().height
-        const elementHeight = el.getBoundingClientRect().width
-        const fontSize = (Math.min(currentFontSize * elementWidth / textWidth, currentFontSize * elementHeight / textHeight) * 0.8).toFixed(2)
-        console.log(`${textAtom.get()} current ${currentFontSize}, target ${fontSize}, width ${textWidth}/${elementWidth}, height ${textHeight}/${elementHeight}`)
-        return fontSize + unit
-      }
-      return "1em"
+    const fontSize = L.view(item, i => {
+      const text = i.text
+      const lines = text.split(/\s/).map(s => s.trim()).filter(s => s).map(s => s.length)
+      const lineCount = Math.max(1, lines.length)
+      const lineLen = Math.max(1, _.max(lines) || 0)
+      const width = i.width
+      const height = i.height
+      const sizeForWidth = width / lineLen * 1.2
+      const sizeForHeight = height / lineCount * 0.8
+      const size = Math.min(sizeForHeight, sizeForWidth)
+
+      return size + "em"
     })
 
     const setEditingIfAllowed = (e: boolean) => {
@@ -104,7 +102,7 @@ export const ItemView = (
       !l[id] && dispatch({ action: "item.lock", boardId: board.get().id, itemId: id })
     }
   
-    return <span className="text" ref={(e : HTMLElement) => setTimeout(() => textElement.set(e), 0)} style={ L.combineTemplate({fontSize}) }>
+    return <span className="text" style={ L.combineTemplate({fontSize}) }>
       <EditableSpan {...{
         value: textAtom, editingThis: L.atom(
             L.view(itemFocus, f => f === "editing"),
