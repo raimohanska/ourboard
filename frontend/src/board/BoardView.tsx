91a7ee2cf77a9695d401a79f5f81f47f81f69734
Juha Paananen <juha.paananen@gmail.com>
2020-11-02 19:19:35 +0200

    prevent backspace=back behavior on Firefox

--- frontend/src/board/BoardView.tsx
+++ frontend/src/board/BoardView.tsx
@@ -54,13 +54,18 @@ export const BoardView = (
     imageUploadHandler(el, assets, coordinateHelper, focus, onAdd, onURL)
   }
 
-  L.fromEvent<JSX.KeyboardEvent>(document, "keyup").pipe(L.applyScope(componentScope())).forEach(e => {
-    if (e.keyCode === 8 || e.keyCode === 46) { // del or backspace
-      const s = focus.get()
-      if (s.status === "selected") {
-        s.ids.forEach(id => dispatch({ action: "item.delete", boardId, itemId: id}))
-      }      
-    }
+  ["keydown", "keyup", "keypress"].forEach(eventName => { // Prevent default for all of these to prevent Backspace=Back behavior on Firefox
+    L.fromEvent<JSX.KeyboardEvent>(document, eventName).pipe(L.applyScope(componentScope())).forEach(e => {
+      if (e.keyCode === 8 || e.keyCode === 46) { // del or backspace
+        e.preventDefault()   
+        if (eventName === "keyup") {
+          const s = focus.get()
+          if (s.status === "selected") {
+            s.ids.forEach(id => dispatch({ action: "item.delete", boardId, itemId: id}))
+          }        
+        }   
+      }
+    })  
   })
 
 
