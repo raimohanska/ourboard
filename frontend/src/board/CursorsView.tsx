bcf23f76a6262656aedddfed4f04c4fd4ff55837
Juha Paananen <juha.paananen@gmail.com>
2021-02-03 16:08:24 +0200

    Improve CursorsView performance: don't re-create all nodes on cursor move

--- frontend/src/board/CursorsView.tsx
+++ frontend/src/board/CursorsView.tsx
@@ -7,27 +7,27 @@ export const CursorsView = (
     { sessions, cursors, coordinateHelper }: 
     { cursors: L.Property<UserCursorPosition[]>, coordinateHelper: BoardCoordinateHelper, sessions: L.Property<UserSessionInfo[]> }
 ) => {
-    return <ListView
+    return <ListView<UserCursorPosition, string>
           observable={cursors}
-          renderObservable={({ x, y, userId }: UserCursorPosition) => <span
-            className="cursor"
-            style={{
+          renderObservable={(userId: string, pos: L.Property<UserCursorPosition>) => {
+            const style = L.combineTemplate({
               position: "absolute", 
               display: "block", 
-              left: coordinateHelper.getClippedCoordinate(x, 'clientWidth', 0) + "em",
-              top: coordinateHelper.getClippedCoordinate(y, 'clientHeight', 2) + "em"
-            }}
-          >
-            <span className="arrow" style={{ 
-              transform: "rotate(-35deg)", 
-              display: "block", 
-              width: "0px", height:"0px", 
-              borderLeft: "5px solid transparent", 
-              borderRight: "5px solid transparent", 
-              borderBottom: "10px solid tomato", 
-            }}/>
-            <span className="text">{sessions.get().find(s => s.userId === userId)?.nickname || null}</span>
-          </span> }
-          getKey={(c: UserCursorPosition) => c}
+              left: L.view(pos, p => p.x, x => coordinateHelper.getClippedCoordinate(x, 'clientWidth', 0) + "em"),
+              top: L.view(pos, p => p.y, y => coordinateHelper.getClippedCoordinate(y, 'clientHeight', 2) + "em")
+            })
+            return  <span className="cursor" style={style}>
+              <span className="arrow" style={{ 
+                transform: "rotate(-35deg)", 
+                display: "block", 
+                width: "0px", height:"0px", 
+                borderLeft: "5px solid transparent", 
+                borderRight: "5px solid transparent", 
+                borderBottom: "10px solid tomato", 
+              }}/>
+              <span className="text">{sessions.get().find(s => s.userId === userId)?.nickname || null}</span>
+            </span>
+          } }
+          getKey={(c: UserCursorPosition) => c.userId}
         />
 }
