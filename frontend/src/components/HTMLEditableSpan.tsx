d7dec0c508149db208b84a9c03029f1f514066b3
Juha Paananen <juha.paananen@gmail.com>
2021-02-08 08:31:45 +0200

    remove LinkMenu as it leaked listeners (clickOutside) and it turns out to be a bad idea to mix managed elements inside an content-editable

--- frontend/src/components/HTMLEditableSpan.tsx
+++ frontend/src/components/HTMLEditableSpan.tsx
@@ -37,7 +37,6 @@ export const HTMLEditableSpan = ( props : EditableSpanProps) => {
         const e = nameElement.get()
         if (!e) return
         e.innerHTML = sanitizeHTML(value.get(), true)
-        createWidgets()
     }
 
     L.combine(value.pipe(L.applyScope(componentScope())), nameElement, (v, e) => ({v, e})).forEach(({v, e }) => {
@@ -48,17 +47,6 @@ export const HTMLEditableSpan = ( props : EditableSpanProps) => {
     })
     editingThis.pipe(L.changes, L.filter(e => !e), L.applyScope(componentScope())).forEach(updateContent)
 
-    const createWidgets = () => {
-        nameElement.get()!.childNodes.forEach(childNode => {
-            if (childNode instanceof HTMLAnchorElement) {
-                const anchorElement = childNode
-                // replace anchor elem with the component
-                H.mount(<LinkMenu href={anchorElement.href} text={anchorElement.textContent!} onInput={onInput} onEdit={() => editingThis.set(true)}/>, anchorElement) 
-                // TODO: the mount is never unmounted. However, Harmaja should find this component when unmounting a larger context.                
-            }
-        })
-    }
-
     const onBlur = (e: JSX.FocusEvent) => {
         //editingThis.set(false)
     }
@@ -66,10 +54,10 @@ export const HTMLEditableSpan = ( props : EditableSpanProps) => {
         e.stopPropagation() // To prevent propagating to higher handlers which, for instance prevent defaults for backspace
     }
     const onClick = (e: JSX.MouseEvent) => {
-        e.preventDefault()
         e.stopPropagation()
+        if (e.target instanceof HTMLAnchorElement) return // Allow links to work
+        e.preventDefault()
         if (e.shiftKey) return
-        if (e.target instanceof HTMLAnchorElement) return
         editingThis.set(true)
     }      
     const onKeyDown = (e: JSX.KeyboardEvent) => {
@@ -108,9 +96,8 @@ export const HTMLEditableSpan = ( props : EditableSpanProps) => {
         e.preventDefault();
         // Paste as plain text, remove formatting.
         var htmlText = e.clipboardData.getData('text/plain');            
-        const sanitized = sanitizeHTML(htmlText, true) // also linkifies, but doesn't add the link menu
-        document.execCommand("insertHTML", false, sanitized);
-        createWidgets() // to create the LinkMenu for the new link 
+        const sanitized = sanitizeHTML(htmlText, true) // also linkifies, that the 'true' :)
+        document.execCommand("insertHTML", false, sanitized);        
     }
 
     return <span 
@@ -163,85 +150,4 @@ function getSelectionTextInfo(el: HTMLElement) {
         atEnd = (testRange.toString() == "");
     }
     return { atStart: atStart, atEnd: atEnd };
-}
-
-const LinkMenu = ({ href, text, onInput, onEdit }: { href: string, text: string, onInput: () => void, onEdit: () => void}) => {
-    // TODO: implement edit
-    // TODO: write about Harmaja integration into vanilla!
-    const element = L.atom<HTMLElement | null>(null)
-    const status = L.atom<"linked"|"menu"|"edit"|"unlinked">("linked")        
-    const onLinkClick = (e: JSX.MouseEvent) => {
-        status.modify(s => s == "menu" ? "linked" : "menu")
-        onEdit()
-        e.preventDefault()
-    }
-    const unlink = () => {
-        status.set("unlinked")
-        onInput() // sync content
-    }
-    const edit = () => {
-        status.set("edit")
-    }
-    onClickOutside(element, () => {
-        status.modify(s => s == "unlinked" ? s : "linked")
-        onInput() // sync content
-    })
-    const nameEdit = L.atom(text)
-    const hrefEdit = L.atom(href)
-
-    return <span className="anchor-container" contentEditable={false} ref={e => { element.set(e); stopEventPropagation(e) }}>
-        {
-            L.view(status, s => s == "unlinked"
-                ? nameEdit
-                : <>
-                { (s == "menu" || s == "edit") && <div className={L.view(status, s => s ? "anchor-menu" : "anchor-menu hidden")}>
-                    <a href={hrefEdit} target="_blank">{ nameEdit }</a>
-                    <a onClick={e => { navigator.clipboard.writeText(hrefEdit.get()) }} className="icon copy" title="Copy link"/>
-                    <a onClick={edit} className="icon edit" title="Edit link"/>
-                    <a onClick={unlink} className="icon unlink" title="Unlink"/>
-
-                    { s === "menu"
-                        ? <>
-                        </>
-                        : <div className="anchor-edit" 
-                                onClick={e => e.stopPropagation()} 
-                                onMouseDown={e => e.stopPropagation()}
-                            >
-                            <label>Text <TextInput value={nameEdit}/></label>
-                            <label>URL <TextInput value={hrefEdit}/></label>
-                          </div>
-                    }
-                </div> }                 
-                <a href={hrefEdit} target="_blank" onClick={onLinkClick}>{ nameEdit }</a>
-                </>
-            )
-        }         
-    </span>
-}
-
-export const TextInput = (props: { value: L.Atom<string> } & any) => {
-    return <input {...{
-        type: props.type || "text" ,
-        onInput: e => {
-            props.value.set(e.currentTarget.value)
-            e.stopPropagation()
-        },
-        ...props,
-        value: props.value
-    }} />
-};
-
-const stopEventPropagation = (el: HTMLElement) => {
-    ["onclick", "onmousedown", "onmouseup", "onkeydown", "onkeyup", "onkeypress", "ondragstart", "ondrag", "ondragend", "oninput", "onblur"].forEach(name => {
-        (el as any)[name] = (e: Event) => {
-            console.log("BLOCKING", name)
-            e.stopPropagation()
-        }
-    })
-}
-
-const EventBarrier = ({ children }: { children: any }) => {
-    return <span ref={stopEventPropagation}>
-        { children }
-    </span>
 }
