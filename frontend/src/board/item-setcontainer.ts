edeee8e0cab9057e9fe50fc1ad9294b8bcc41e2e
Juha Paananen <juha.paananen@gmail.com>
2021-02-21 12:03:55 +0200

    only allow containment by Container items in maybeChangeContainer. Otherwise notes may end up containing each other

--- frontend/src/board/item-setcontainer.ts
+++ frontend/src/board/item-setcontainer.ts
@@ -4,7 +4,8 @@ import { containedBy } from "./geometry"
 export function maybeChangeContainer(item: Item, b: Board): Item | undefined {
     const currentContainer = item.containerId && b.items.find((i) => i.id === item.containerId)
     if (currentContainer && containedBy(item, currentContainer)) return currentContainer
-    return b.items.find((i) => i.id !== item.id && containedBy(item, i))
+    if (item.type === "container") return undefined
+    return b.items.find((i) => i.type === "container" && i.id !== item.id && containedBy(item, i))
 }
 
 export function withCurrentContainer(item: Item, b: Board): Item {
