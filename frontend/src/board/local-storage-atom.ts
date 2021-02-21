47d114fd4a3c9f7878e63366c9b3875b6da4cb43
jussisaurio <jussi.saurio@gmail.com>
2021-02-21 17:45:55 +0200

    fix localStorageAtom retval being inferred as 'any'

--- frontend/src/board/local-storage-atom.ts
+++ frontend/src/board/local-storage-atom.ts
@@ -2,7 +2,7 @@ import * as L from "lonna"
 
 export function localStorageAtom<T>(key: string, defaultValue: T) {
     const initialValue = localStorage[key] !== undefined ? JSON.parse(localStorage[key]) : defaultValue
-    const atom = L.atom(initialValue)
+    const atom = L.atom<T>(initialValue)
     atom.onChange((v) => (localStorage[key] = JSON.stringify(v)))
     return atom
 }
