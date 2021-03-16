bf173c43a489c8445ebb093070110310eb39bd81
Juha Paananen <juha.paananen@gmail.com>
2021-03-16 14:15:09 +0200

    don't abort compaction for single bundle. Still makes sense to fix broken history.

--- backend/src/compact-history.ts
+++ backend/src/compact-history.ts
@@ -21,85 +21,77 @@ import { mkBootStrapEvent, migrateBoard } from "../../common/src/migration"
 export function compactBoardHistory(id: Id) {
     return inTransaction(async (client) => {
         const bundles = await getBoardHistoryBundles(client, id)
-        if (bundles.length == 1) {
-            console.log(
-                `Board ${id}: 1 bundle containing ${bundles[0].events.events.length} events => no need to compact`,
-            )
-        } else {
-            const eventArrays = bundles.map((b) => b.events.events)
-            const consistent = verifyContinuity(id, 0, ...eventArrays)
-            const events: BoardHistoryEntry[] = eventArrays.flat()
-            if (consistent) {
-                const groups = Object.values(_.groupBy(events, (e) => Math.floor(e.serial! / 1000)))
-                const sorted: DateBundle[] = []
-                let current: DateBundle | null = null
-                type DateBundle = { date: string; events: BoardHistoryEntry[] }
-                for (let event of events) {
-                    const date = format(new Date(event.timestamp), "yyyy-MM-dd hh")
-                    if (current == null || date !== current.date) {
-                        if (current !== null) {
-                            sorted.push(current)
-                        }
-                        current = { events: [], date }
+        const eventArrays = bundles.map((b) => b.events.events)
+        const consistent = verifyContinuity(id, 0, ...eventArrays)
+        const events: BoardHistoryEntry[] = eventArrays.flat()
+        if (consistent) {
+            const groups = Object.values(_.groupBy(events, (e) => Math.floor(e.serial! / 1000)))
+            const sorted: DateBundle[] = []
+            let current: DateBundle | null = null
+            type DateBundle = { date: string; events: BoardHistoryEntry[] }
+            for (let event of events) {
+                const date = format(new Date(event.timestamp), "yyyy-MM-dd hh")
+                if (current == null || date !== current.date) {
+                    if (current !== null) {
+                        sorted.push(current)
                     }
-                    current.events.push(event)
+                    current = { events: [], date }
                 }
-                if (current != null) {
-                    sorted.push(current)
-                }
-                const compactedOk = verifyContinuity(id, 0, ...sorted.map((e) => e.events))
-                if (!compactedOk || sorted.flatMap((e) => e.events).length !== events.length) {
-                    throw Error("Compaction failure")
-                }
-                if (sorted.length === bundles.length) {
-                    console.log(
-                        `Board ${id}: Verified ${bundles.length} bundles containing ${events.length} events => no need to compact`,
-                    )
-                    return
-                }
+                current.events.push(event)
+            }
+            if (current != null) {
+                sorted.push(current)
+            }
+            const compactedOk = verifyContinuity(id, 0, ...sorted.map((e) => e.events))
+            if (!compactedOk || sorted.flatMap((e) => e.events).length !== events.length) {
+                throw Error("Compaction failure")
+            }
+            if (sorted.length === bundles.length) {
                 console.log(
-                    `Board ${id}: Verified ${bundles.length} bundles containing ${events.length} events => compacting to ${sorted.length} bundles`,
+                    `Board ${id}: Verified ${bundles.length} bundles containing ${events.length} events => no need to compact`,
                 )
-                //console.log(sorted.map(s => s.date).join("\n  "))
+                return
+            }
+            console.log(
+                `Board ${id}: Verified ${bundles.length} bundles containing ${events.length} events => compacting to ${sorted.length} bundles`,
+            )
+            //console.log(sorted.map(s => s.date).join("\n  "))
 
-                const serials = bundles.map((b) => b.last_serial!)
-                const result = await client.query(
-                    `DELETE FROM board_event where board_id=$1 and last_serial in (${serials.join(",")})`,
-                    [id],
-                )
-                if (result.rowCount != serials.length) {
-                    throw Error(`Unexpected rowcount ${result.rowCount} for board ${id}`)
-                }
-                for (let newBundle of sorted) {
-                    await storeEventHistoryBundle(id, newBundle.events, client)
+            const serials = bundles.map((b) => b.last_serial!)
+            const result = await client.query(
+                `DELETE FROM board_event where board_id=$1 and last_serial in (${serials.join(",")})`,
+                [id],
+            )
+            if (result.rowCount != serials.length) {
+                throw Error(`Unexpected rowcount ${result.rowCount} for board ${id}`)
+            }
+            for (let newBundle of sorted) {
+                await storeEventHistoryBundle(id, newBundle.events, client)
+            }
+        } else {
+            console.warn(`Aborting compaction of board ${id} due to inconsistent history`)
+            const result = await client.query("SELECT content, history FROM board WHERE id=$1", [id])
+            if (result.rowCount != 1) {
+                console.warn("Board not found!?")
+                return
+            }
+            const snapshot = result.rows[0].content as Board
+            if (snapshot.serial) {
+                console.log(`Found snapshot at serial ${snapshot.serial}`)
+
+                const followingEvents = events.filter((e) => e.serial! > snapshot.serial)
+
+                if (followingEvents.length > 0 && followingEvents[0].serial !== snapshot.serial + 1) {
+                    console.log(
+                        `Cannot find a consecutive event for snapshot. First event is ${followingEvents[0]?.serial}`,
+                    )
+                } else {
+                    console.log("Bootstraping history based on snapshot")
+                    await bootstrapHistory(id, snapshot, followingEvents, client)
                 }
             } else {
-                console.warn(`Aborting compaction of board ${id} due to inconsistent history`)
-                const result = await client.query("SELECT content, history FROM board WHERE id=$1", [id])
-                if (result.rowCount != 1) {
-                    console.warn("Board not found!?")
-                    return
-                }
-                const snapshot = result.rows[0].content as Board
-                if (snapshot.serial) {
-                    console.log(`Found snapshot at serial ${snapshot.serial}`)
-
-                    const followingEvents = events.filter((e) => e.serial! > snapshot.serial)
-
-                    if (followingEvents.length > 0 && followingEvents[0].serial !== snapshot.serial + 1) {
-                        console.log(
-                            `Cannot find a consecutive event for snapshot. First event is ${followingEvents[0]?.serial}`,
-                        )
-                    } else {
-                        console.log("Bootstraping history based on snapshot")
-                        await bootstrapHistory(id, snapshot, followingEvents, client)
-                    }
-                } else {
-                    console.warn(
-                        "No snapshot with serial available discarding history and bootstrapping to latest state.",
-                    )
-                    await bootstrapHistory(id, mkSnapshot(snapshot, 0), [], client)
-                }
+                console.warn("No snapshot with serial available discarding history and bootstrapping to latest state.")
+                await bootstrapHistory(id, mkSnapshot(snapshot, 0), [], client)
             }
         }
     })
