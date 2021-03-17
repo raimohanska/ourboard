81a10faf2330c0ac90d13e08eb2e20f3f1764cc2
Juha Paananen <juha.paananen@gmail.com>
2021-03-17 18:37:37 +0200

    when multiple containers match, stick to innermost

--- frontend/src/board/item-setcontainer.ts
+++ frontend/src/board/item-setcontainer.ts
@@ -1,11 +1,15 @@
-import { Board, Item } from "../../../common/src/domain"
+import { Board, isContainedBy, Item } from "../../../common/src/domain"
 import { containedBy } from "./geometry"
 
 export function maybeChangeContainer(item: Item, b: Board): Item | undefined {
-    const currentContainer = item.containerId && b.items[item.containerId]
-    if (currentContainer && containedBy(item, currentContainer)) return currentContainer
     if (item.type === "container") return undefined
-    return Object.values(b.items).find((i) => i.type === "container" && i.id !== item.id && containedBy(item, i))
+    const currentContainer = item.containerId && b.items[item.containerId]
+
+    const candidates = Object.values(b.items)
+        .filter((i) => i.type === "container" && i.id !== item.id && containedBy(item, i)) // contain the item coordinate-wise
+        .sort((a, b) => (containedBy(b, a) ? 1 : -1)) // most innermost first (containers last)
+
+    return candidates[0]
 }
 
 export function withCurrentContainer(item: Item, b: Board): Item {
