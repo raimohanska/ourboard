6d16d3b02a1a8dcdb5db91d77135a37bb3c38856
Juha Paananen <juha.paananen@gmail.com>
2021-03-20 18:51:56 +0200

    use same light gray as board background

--- common/src/colors.ts
+++ common/src/colors.ts
@@ -6,7 +6,7 @@ export const PINK = "#FDC4E7"
 export const LIGHT_PURPLE = "#E0BDFA"
 export const RED = "#F62A5C"
 export const BLACK = "#000000"
-export const LIGHT_GRAY = "#EAEAEA"
+export const LIGHT_GRAY = "#f4f4f6"
 export const WHITE = "#ffffff"
 
 export const NOTE_COLORS = [LIGHT_BLUE, LIGHT_GREEN, YELLOW, ORANGE, PINK, LIGHT_PURPLE, RED, BLACK, LIGHT_GRAY, WHITE]
