790b52c170a763664318bdf031ea322fa6eebb8d
Juha Paananen <juha.paananen@gmail.com>
2021-04-03 15:28:53 +0300

    allow specifying several board ids in perftester

--- perf-tester/src/index.ts
+++ perf-tester/src/index.ts
@@ -2,6 +2,7 @@ import { sleep } from "../../common/src/sleep"
 import { newNote, Point } from "../../common/src/domain"
 import MessageQueue from "../../frontend/src/store/message-queue"
 import WebSocket from "ws"
+import _ from "lodash"
 
 // hack, sue me
 // @ts-ignore
@@ -28,6 +29,7 @@ function createTester(nickname: string, boardId: string) {
     }
     function initSocket() {
         let ws: WebSocket
+        const WS_ADDRESS = `${DOMAIN ? "wss" : "ws"}://${DOMAIN ?? "localhost:1337"}/socket/board/${boardId}`
         ws = new WebSocket(WS_ADDRESS)
 
         ws.addEventListener("error", (e) => {
@@ -37,7 +39,7 @@ function createTester(nickname: string, boardId: string) {
         ws.addEventListener("open", () => {
             console.log("Websocket connected")
             messageQueue.onConnect()
-            console.log("Joining board")
+            console.log("Joining board " + boardId)
             messageQueue.enqueue({ action: "board.join", boardId })
         })
         ws.addEventListener("message", (str) => {
@@ -106,9 +108,13 @@ for (let i = 0; i < kwargs.length - 1; i += 2) {
 
 // Environment variables.
 const USER_COUNT = parseInt(process.env.USER_COUNT ?? "10")
-const BOARD_ID = process.env.BOARD_ID ?? "10"
+const BOARD_ID = process.env.BOARD_ID
+if (!BOARD_ID) {
+    throw Error("BOARD_ID missing. Please specify one more board ids separated by comma (,)")
+}
+const BOARD_IDS = BOARD_ID.split(",")
 const DOMAIN = process.env.DOMAIN
-const WS_ADDRESS = `${DOMAIN ? "wss" : "ws"}://${DOMAIN ?? "localhost:1337"}/socket/board/${BOARD_ID}`
+
 const NOTES_PER_SEC = parseFloat(process.env.NOTES_PER_SEC ?? "0.1")
 const CURSOR_MOVES_PER_SEC = parseFloat(process.env.CURSOR_MOVES_PER_SEC ?? "10")
 
@@ -122,5 +128,5 @@ console.log(`Total cursor events ${CURSOR_MOVES_PER_SEC * USER_COUNT}/s`)
 console.log(`Total creation events ${NOTES_PER_SEC * USER_COUNT}/s`)
 
 for (let i = 0; i < USER_COUNT; i++) {
-    createTester("perf-tester-" + (i + 1), BOARD_ID)
+    createTester("perf-tester-" + (i + 1), BOARD_IDS[i % BOARD_IDS.length])
 }
