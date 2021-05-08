8e76a6ec2de8356c39d4755677acced1992d72b9
Juha Paananen <juha.paananen@gmail.com>
2021-05-08 16:57:41 +0300

    make item.front work again for similar sized items

--- frontend/src/board/zIndices.ts
+++ frontend/src/board/zIndices.ts
@@ -5,5 +5,5 @@ export const Z_CONNECTIONS = 200000000
 export const Z_ITEMS_FROM = Z_CONTAINERS_UP_TO + 10
 
 export function itemZIndex(item: Item) {
-    return Math.floor(1000000 - item.width * item.height)
+    return Math.floor(1000000 - item.width * item.height * 10) + item.z
 }
