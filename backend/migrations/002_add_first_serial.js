059db8b6f1b7522ba174b3cfa7aba61cadb0f89d
Juha Paananen <juha.paananen@gmail.com>
2021-10-16 21:19:39 +0300

    coalesce null first_serial into zero (two occurrences in prod)

--- backend/migrations/002_add_first_serial.js
+++ backend/migrations/002_add_first_serial.js
@@ -1,7 +1,7 @@
 exports.up = (pgm) => {
     pgm.sql(`
     ALTER TABLE board_event ADD COLUMN IF NOT EXISTS first_serial int;
-    UPDATE board_event SET first_serial = CAST(events#>'{events, 0, serial}' as int);
+    UPDATE board_event SET first_serial = COALESCE(CAST(events#>'{events, 0, serial}' as int), 0);
     ALTER TABLE board_event ALTER COLUMN first_serial SET NOT NULL;
     `)
 }
