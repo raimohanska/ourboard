7c9d28099c507b7a73d7fce4d30afc172acd4a93
Juha Paananen <juha.paananen@gmail.com>
2022-02-07 16:20:08 +0200

    yarn format

--- frontend/src/board/ItemView.tsx
+++ frontend/src/board/ItemView.tsx
@@ -151,9 +151,13 @@ export const ItemView = ({
             focused,
             coordinateHelper,
             element,
-        )        
+        )
         return (
-            <span className="text" onDoubleClick={e => e.stopPropagation()} style={L.combineTemplate({ fontSize, color })}>
+            <span
+                className="text"
+                onDoubleClick={(e) => e.stopPropagation()}
+                style={L.combineTemplate({ fontSize, color })}
+            >
                 <HTMLEditableSpan
                     {...{
                         value: textAtom,
