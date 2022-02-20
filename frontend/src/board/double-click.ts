65df2f92ecf25832f2457397e2974e2ca964dc3d
Juha Paananen <juha.paananen@gmail.com>
2022-02-20 14:50:38 +0200

    Support dblclick to create on touchscreen

--- frontend/src/board/double-click.ts
+++ frontend/src/board/double-click.ts
@@ -1,11 +1,26 @@
 import { componentScope } from "harmaja"
 import * as L from "lonna"
+import { IS_TOUCHSCREEN } from "./touchScreen"
 
 export function installDoubleClickHandler(action: (e: JSX.MouseEvent) => void) {
-    L.fromEvent<JSX.MouseEvent>(window, "dblclick")
+    if (IS_TOUCHSCREEN) {
+        let previousClick = 0;
+        L.fromEvent<JSX.MouseEvent>(window, "click")
+        .pipe(L.applyScope(componentScope()))
+        .forEach((e) => {
+            const now = new Date().getTime()
+            if (now - previousClick < 300) {
+                action(e)
+            }
+            previousClick = now
+        })
+    } else 
+    {
+        L.fromEvent<JSX.MouseEvent>(window, "dblclick")
         .pipe(L.applyScope(componentScope()))
         .forEach((e) => {
             e.preventDefault()
             action(e)
         })
+    }
 }
