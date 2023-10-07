68b14611487a187443a4c5f2525df5d547177d04
Juha Paananen <juha.paananen@gmail.com>
2023-10-07 22:11:15 +0300

    Zoom a bit faster with them buttonses

--- frontend/src/board/toolbars/ZoomControls.tsx
+++ frontend/src/board/toolbars/ZoomControls.tsx
@@ -5,10 +5,10 @@ import { ZoomAndScrollControls } from "../board-scroll-and-zoom"
 export function ZoomControls({ adjustZoom }: ZoomAndScrollControls) {
     return (
         <span className="zoom-controls">
-            <span className="icon" title="Zoom in" onClick={() => adjustZoom((z) => z * 1.1, "preserveCenter")}>
+            <span className="icon" title="Zoom in" onClick={() => adjustZoom((z) => z * 1.2, "preserveCenter")}>
                 <ZoomInIcon />
             </span>
-            <span className="icon" title="Zoom out" onClick={() => adjustZoom((z) => z / 1.1, "preserveCenter")}>
+            <span className="icon" title="Zoom out" onClick={() => adjustZoom((z) => z / 1.2, "preserveCenter")}>
                 <ZoomOutIcon />
             </span>
         </span>
