1be809a6cc92c045daa7e309023cf84d9c43036f
Juha Paananen <juha.paananen@gmail.com>
2024-01-27 09:56:15 +0200

    Upgrade playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,18 @@
 import { PlaywrightTestConfig } from "@playwright/test"
 
 const ci = process.env.CI === "true"
-const headless = ci || process.env.HEADLESS === "true"
 
 const config: PlaywrightTestConfig = {
     testDir: "playwright",
+    outputDir: "playwright/results",
+    fullyParallel: true,
+    workers: ci ? 2 : 4,
+    forbidOnly: ci,
     timeout: 60000, // Timeout per test file (default 30000)
     use: {
-        headless,
         baseURL: "http://localhost:8080",
         actionTimeout: 15000,
+        trace: "on",
     },
     reporter: ci ? "github" : "line",
 }
