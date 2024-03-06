ff00c4d943b4037ce6f33cc55b6104cd8a5d4c9c
Juha Paananen <juha.paananen@gmail.com>
2024-03-06 18:22:43 +0200

    Add Playwright retries on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,6 +9,7 @@ const config: PlaywrightTestConfig = {
     workers: ci ? 2 : 4,
     forbidOnly: ci,
     timeout: 60000, // Timeout per test file (default 30000)
+    retries: ci ? 2 : 0,
     use: {
         baseURL: "http://localhost:8080",
         actionTimeout: 15000,
